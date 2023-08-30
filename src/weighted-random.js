bbc328721b1a31a0b381a34cfe3e003dcec0a886
John Evans <john@wearelighthouse.com>
2023-08-30 16:11:07 +0100

    Add missing weighted-random file

---
+++ src/weighted-random.js
@@ -0,0 +1,13 @@
+export const weightedRandom = (weights) => {
+  const totalWeight = weights.reduce((sum, weight) => sum + weight, 0);
+  const randomValue = Math.random() * totalWeight;
+  let cumulativeWeight = 0;
+
+  for (let i = 0; i < weights.length; i++) {
+    cumulativeWeight += weights[i];
+
+    if (randomValue < cumulativeWeight) {
+      return i;
+    }
+  }
+};
